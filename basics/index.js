40bc94dc5f3cd4d61ca411278ec119afde6514a4
ikhlaasgooly <goolyikhlaas@gmail.com>
2024-03-17 17:49:00 +0400

    basics

---
+++ basics/index.js
@@ -0,0 +1,18 @@
+// Print 'hello' to the console
+console.log(`hello`);
+
+// Print 'i like pizza' to the console
+console.log(`i like pizza`);
+
+// Create a pop-up alert box with the message 'This is an alert'
+window.alert(`This is an alert`);
+
+// Create a pop-up alert box with the message 'I like pizza'
+window.alert(`I like pizza`);
+
+
+// Set the text content of the element with the id 'myh1' to 'HELLO'
+document.getElementById("myh1").textContent = 'HELLO';
+
+// Set the text content of the element with the id 'mypara' to 'I like pizza'
+document.getElementById("mypara").textContent = 'I like pizza';
