3e4177ad1b509d6189860ad0819990dde3c718ea
ikhlaasgooly <goolyikhlaas@gmail.com>
2024-03-19 20:05:12 +0400

    arithmetic operator

---
+++ arithmetic operator/index.js
@@ -0,0 +1,28 @@
+let student = 31;
+
+//student = student + 1;  // addition
+//student = student - 1;  // substraction
+//student = student * 2;  // multiplication
+//student = student / 2;  // division
+//student = student ** 2; // power
+// student = student % 2; // modulus
+
+// Or
+//student += 1;
+//student -= 1;
+//student *= 2;
+//student /= 2;
+//student **= 2;
+//student %= 2;
+
+//console.log(student);
+
+
+// Operator precedence (BODMAS)
+
+let result = 1 + 2 * 3 + 4 **2;
+
+console.log(result);
+
+
+
